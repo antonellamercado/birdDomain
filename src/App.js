e6326faa616a599331a6d2b68ea44fe4fda70a72
GM <slipknokita@gmail.com>
2020-11-15 23:33:49 -0300

    navbar con estilo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Map from './components/Maps/MapT1'
+// import Map from './components/Maps/MapT1'
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
+import MyNav from './components/Header/MyNav.js'
 import Home from './views/Home/Home';
 import Destacado from './views/Destacado/Destacado';
 import Contacto from './views/Contacto/Contacto';
 import Favoritos from './views/Favoritos/Favoritos';
 import Carrito from './views/Carrito/Carrito';
 
+
 function App() {
   return (
     <div className="App">
     <Router>
-      <Header/>
-      <div ClassName="app">
+      <MyNav/>
+      <div className="app">
          <Switch>
          <Route path = "/" exact component = {Home}></Route>
            <Route path = "/Destacado" exact component = {Destacado}></Route>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import img from './Tour2.jpg'
 
 const Home = () => {
 
     return (
         <Link to = '/'>
-<div>Home</div>
+<div>Home
+</div>
+<img className="container-fluid" src = {img} alt = "..."></img>
         </Link>
     );
 }
