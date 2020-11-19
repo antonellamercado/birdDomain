1ded437487ce341465cfd8d4a599b8db0db7de26
Antonella Mercado <antonellamercado@gmail.com>
2020-11-19 19:33:45 -0300

    galeria funcionando

--- src/components/Galeria/Galeria.js
+++ src/components/Galeria/Galeria.js
@@ -13,38 +13,43 @@ const Galeria = () => {
 const [galeria, setGaleria] = useState([
    { }
 ]);
+const [imgChica, setimgChica] = useState();
+const [imgMediana, setimgMediana] = useState();
+const [imgGrande, setimgGrande] = useState();
+
 
 useEffect(()=>{
-    const getGaleria = async ()=>{
-    await clienteAxios.get("/Aves")
+        const getGaleria = async ()=>{
+        await clienteAxios.get("/Aves")
     .then(response =>{
     setGaleria(response.data)
-    });
-    }
-    getGaleria();
-    },[]);
-    console.log(galeria);
+});
+}
+getGaleria();
+},[]);
+console.log(galeria);
 
 
     return (
-<div  className='cardContainer d-flex justify-content-center row mx-0'>
-
+        <div  className='cardContainer d-flex justify-content-center row mx-0'>
                 {                             
                     galeria.length === 0 ? 'No hay Aves disponible' : 
                     (galeria.map((ave, index) =>
-                        <Link className="ml-3 mr-3 mb-3 newCardsResults col-xs-12 col-sm-12 col-md-6 col-lg-3">
+                        <Link className=" mb-3 newCardsResults col-xs-12 col-sm-12 col-md-4 col-lg-3 m-0">
                         <Card>
                         <CardBody>
                             <div key={index} className="">
                                 <CardImg className="" width="20%" src={ave.img}  alt="img-aves"/>
                                 <div className="card-img-overlay">
-                                <CardTitle className="text-muted">{ave.title}</CardTitle>
+                                <CardTitle className="detalle_ave">{ave.title}</CardTitle>
                                 </div>
                             </div>
                         </CardBody>
                         </Card>
                         </Link> 
+                        
                     ))
+                   
                 } 
         
 </div>
