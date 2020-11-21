f62411abf7c228d8c008aac16ed882c361142782
Antonella Mercado <antonellamercado@gmail.com>
2020-11-21 17:29:41 -0300

    galeria con dos columnas - falta estilo

--- src/components/Galeria/Galeria.js
+++ src/components/Galeria/Galeria.js
@@ -1,65 +1,23 @@
-import {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
 //component
 import ImagenMediana from '../ImagenMediana/ImagenMediana';
-//import ImagenGrande from '../ImagenMediana/ImagenGrande';
+import ImagenPequeña from '../ImagenPequeña/ImagenPequeña';
 //config
-import clienteAxios from '../../config/axios';
 //style
 import '../Galeria/Galeria.css';
 //libreria
-import {Card, CardBody, CardImg, CardTitle} from 'reactstrap';
+
 
 
 const Galeria = () => {
 
-const [galeria, setGaleria] = useState([
-   { }
-]);
-//const [imgChica, setimgChica] = useState();
-//const [imgGrande, setimgGrande] = useState();
+return (
 
-
-useEffect(()=>{
-        const getGaleria = async ()=>{
-        await clienteAxios.get("/Aves")
-    .then(response =>{
-    setGaleria(response.data)
-});
-}
-getGaleria();
-},[]);
-console.log(galeria);
-
-
-    return (
-
-        <div className="d-flex justify-content-center row mx-0">
-          <ImagenMediana/>  
-          <div  className='cardContainer col-lg-6 flex-wrap'>
-           
-                {                             
-                    galeria.length === 0 ? 'No hay Aves disponible' : 
-                    (galeria.map((ave, index) =>
-                        <Link className=" mb-3 newCardsResults col-xs-12 col-sm-12 col-md-4 col-lg-3 m-0">
-                        <Card>
-                        <CardBody>
-                            <div key={index} className="">
-                                <CardImg className="" width="10%" src={ave.img}  alt="img-aves"/>
-                                <div className="card-img-overlay">
-                                <CardTitle className="detalle_ave">{ave.title}</CardTitle>
-                                </div>
-                            </div>
-                        </CardBody>
-                        </Card>
-                        </Link>         
-                    ))      
-                } 
-             
-        </div>
-        </div>
+    <div className="d-flex row">
+        <ImagenMediana/>  
+        <ImagenPequeña/>
+    </div>
         
-            );
+        );
 }
 
 export default Galeria;
--- src/components/ImagenMediana/ImagenMediana.js
+++ src/components/ImagenMediana/ImagenMediana.js
@@ -2,19 +2,21 @@ import {Card, CardBody, CardImg, CardTitle} from 'reactstrap';
 //component
 //config
 //style
+import '../ImagenMediana/ImagenMediana.css';
 //libreria
 
 
 const ImagenMediana = () => {
 
     return(
-        <div>
-    <Card className="col-lg-6 m-0">
-        <CardBody>
-            <CardImg  width="80%" src="https://firebasestorage.googleapis.com/v0/b/bd-aves.appspot.com/o/Fig12.jpg?alt=media&token=b7a63dea-8285-4d00-b016-469d4fa47054" />
-        </CardBody>
-    </Card>
-</div>
+    <div className="d-flex row mx-0 col-lg-6  col-md-6">
+        <Card  className="col-lg-6  col-md-6 m-0 imgMediana">
+            <CardBody>
+            <CardTitle>Imagen Mediana</CardTitle>
+            <CardImg  width="" src="https://firebasestorage.googleapis.com/v0/b/bd-aves.appspot.com/o/Fig12.jpg?alt=media&token=b7a63dea-8285-4d00-b016-469d4fa47054" />
+            </CardBody>
+        </Card>
+    </div>
     );
 
 }
