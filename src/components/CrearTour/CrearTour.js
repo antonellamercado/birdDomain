4308dce511a1b6a2b448ac52db7a33ca5d590da6
Antonella Mercado <antonellamercado@gmail.com>
2020-11-22 15:40:50 -0300

    trabajando pag admin - incompleto

--- src/components/CrearTour/CrearTour.js
+++ src/components/CrearTour/CrearTour.js
@@ -9,52 +9,62 @@ const CrearTour = () => {
     const [modal, setModal] = useState(false);
     const [nuevoTour, setNuevoTour] = useState({});
     const toggle = () => setModal(!modal);
+
+    const handleOnChange = e => {
+        setNuevoTour({
+            ...nuevoTour,
+            [e.target.name]: e.target.value
+        })
+    }
+    console.log('nuevo tour', nuevoTour);
 //const   showModalCrearTour = () => {
-    useEffect (()=> { 
-        const postNewTour = async()  =>{
-            await clienteAxios.post("/Tours")
+    const handleOnSubmit = async e => {
+        e.preventDefault();
+        try {
+            await clienteAxios.post('/Tours', {nuevoTour})
             .then(response =>{
-            setNuevoTour(response.data)
-            });
-            }
-            postNewTour();         
-        },[]);
-
-
+            console.log('nuevo post', response.data)
+            } )
+        }
+        catch (error) {
+            console.log(error.response);
+        }
+    } 
+        
     return (
     <>
         <Button onClick={toggle}>Crear tour</Button>
         <Modal isOpen={modal} toggle={toggle} className="formulario_modal">
             <ModalHeader toggle={toggle}>Nuevo Tour</ModalHeader>
             <ModalBody className="formulario_modal">
-                <Form className="formulario_modal  m-1 p-2">
+                <Form className="formulario_modal  m-1 p-2" onSubmit={handleOnSubmit}>
                     <FormGroup>
                         <Label>Titulo</Label>
-                        <Input type="text"></Input>
+                        <Input type="text" name="title" onChange={handleOnChange}></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label>Descripcion</Label>
-                        <Input type="textarea"></Input>
+                        <Input type="textarea" name="body" onChange={handleOnChange}></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label>Imagen Portada Tour</Label>
-                        <Input type="text"></Input>
+                        <Input type="text" name="img" onChange={handleOnChange}></Input>
                     </FormGroup>
                     <FormGroup>
                         <Label>Imagen Ave destacada</Label>
-                        <Input type="text"></Input>
+                        <Input type="text" name="imgD" onChange={handleOnChange}></Input>
                     </FormGroup>
                     <Row>
                     <Col  md={6}>
                     <FormGroup>
                         <Label>Precio</Label>
-                        <Input type="text"></Input>
+                        <Input type="text" name="price" onChange={handleOnChange}></Input>
                     </FormGroup>
                     </Col>
                     <Col  md={6}>
                     <FormGroup>
                         <Label>Cantidad de dias</Label>
-                        <Input type="text"></Input>
+                        <Input type="text" name="dias" onChange={handleOnChange}></Input>
                     </FormGroup>
                     </Col>
                     </Row>
@@ -62,16 +72,34 @@ const CrearTour = () => {
                     <Col  md={6}>
                     <FormGroup>
                         <Label>Ecoregion</Label>
-                        <Input type="text"></Input>
+                        <Input type="text" name="ecoregiones" onChange={handleOnChange}></Input>
                     </FormGroup>
                     </Col>
                     <Col  md={6}>
                     <FormGroup>
                         <Label>Cantidad de especies</Label>
-                        <Input type="text"></Input>
+                        <Input type="text" name="especies"  onChange={handleOnChange}></Input>
                     </FormGroup>
                     </Col>
                     </Row>
+                    <FormGroup row>
+                        <Label for="exampleSelectMulti" sm={2}>Aves en avistaje</Label>
+                        <Col sm={10}>
+                        <Input type="select" name="aves" id="exampleSelectMulti" multiple onChange={handleOnChange}>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Input>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup check>
+                        <Label check>
+                        <Input type="checkbox" />{' '}
+                        Destacada
+                        </Label>
+                    </FormGroup>
                 </Form>
             </ModalBody>
             <ModalFooter>
